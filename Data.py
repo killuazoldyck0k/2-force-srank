ba408c63c47e2c3fd7f41f51f4f1e38087222b24
kaustubh090 <126164591+kaustubh090@users.noreply.github.com>
2023-11-03 01:44:21 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -6,26 +6,9 @@ from pyrogram.types import InlineKeyboardButton
 
 class Data:
     HELP = """
-<b>How to Use this Bot
 
-  ❏ Commands for BOT Users
-  ├ /start - Starts the Bot
-  ├ /about - About this Bot
-  ├ /help - Help this Bot Command
-  ├ /ping - To check live bots
-  └ /uptime - To see bot status
-
-  ❏ Commands For BOT Admins
-  ├ /logs - To view bot logs
-  ├ /setvar - To set var with dibot command
-  ├ /delvar - To remove var with dibot command
-  ├ /getvar - To see one of the var with dibot command
-  ├ /users - To view bot user statistics
-  ├ /batch - To link more than one file
-  ├ /speedtest - To test the bot server speed
-  └ /broadcast - To send a broadcast message to the bot user
-  
- 👨‍💻 Developed by </b><a href='https://t.me/killua_og'>ɢ ᴏ ᴅㅤ ᴏ ꜰㅤ ꜱ ᴘ ᴇ ᴇ ᴅ</a>
+<b>Press "Start"</b>
+Subscribe☑️ to the channel and select the Series you want to watch📲
 """
 
     close = [
@@ -47,9 +30,7 @@ class Data:
     ]
 
     ABOUT = """
-○ <b>Channel</b> : @Anime_Alliance
-○ <b>Request</b> : @newanimerequestbot
-○ <b>Chatting Group</b> : @Alliance_Weebs
+Hello , Thanks for using me :D
 
-♥︎ Developed by @Anime_Alliance
+♥️ Developed by Unkown entity (≧▽≦)
 """
